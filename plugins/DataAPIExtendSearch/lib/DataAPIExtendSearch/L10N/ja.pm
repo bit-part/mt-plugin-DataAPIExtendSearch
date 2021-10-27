bc56045938dba0b6810492ed9181023b90952a67
Roy Okuwaki <info@tinybeans.dev>
2021-10-28 11:27:28 +1300

    upgrade: Released v1.2.0

--- plugins/DataAPIExtendSearch/lib/DataAPIExtendSearch/L10N/ja.pm
+++ plugins/DataAPIExtendSearch/lib/DataAPIExtendSearch/L10N/ja.pm
@@ -4,8 +4,10 @@ use base 'DataAPIExtendSearch::L10N::en_us';
 use vars qw ( %Lexicon );
 
 %Lexicon = (
-  'bit part LLC' => 'bit part 合同会社',
-	'Add filters by Tag, Category and Date to Data API Endpoint of Entries.' => 'Data APIのEntriesのエンドポイントに、タグフィルター、カテゴリフィルターおよびに付けフィルターを追加します。',
+    'bit part LLC'                                                                             => 'bit part 合同会社',
+    'Add filters by Tag, Category and Date to Data API Endpoint of Entries.'                   => 'Data APIのEntriesのエンドポイントに、タグフィルター、カテゴリフィルターおよびに付けフィルターを追加します。',
+    'This plugin gives some additional filters to retrieve a list of content data or entries via Data API.'
+                                                                                               => 'Data API で記事やコンテンツデータの一覧を取得するためのフィルターを追加するプラグインです。',
 );
 
 1;
